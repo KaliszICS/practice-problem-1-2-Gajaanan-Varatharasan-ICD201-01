4faf93c4aa587312f4b4c12113924193f0f1e832
Gajaanan-Varatharasan-ICD201-01 <gajaanan.varatharasan@student.tdsb.on.ca>
2024-11-04 11:04:02 -0500

    Update PP1_2.py

--- PP1_2.py
+++ PP1_2.py
@@ -2,31 +2,43 @@
 
 #Write Assignment code here
 
-
-name="Alex"
-print(name)
-
-num1=3
-num2=2
-num3=5
+def q1():
+  name="Alex"
+  print(name)
+def q2():
+  num1=3
+  num2=2
+  num3=5
 print(f"sum of all numbers is {num1+num2+num3}")
+def q3():
+  num1=21
+  num2=4
+  print(f"quotient of the two numbers is {num1//num2}")
+  
+def q4():
+  num1=6.3
+  num2=3.0
+  print(f"quotient of two numbers is {num1/num2}")
+  
+def q5():
+  word="Hello World"
+  print(word)
+  word="Goodbye World"
+  print(word)
+  
+def q6():
+  value=True
+  print(value)
+  
+def q7():
+  num1=7
+  num2=2
+  print(f"remainder of two numbers is {num1%num2}")
 
-num1=21
-num2=4
-print(f"quotient of the two numbers is {num1//num2}")
-
-num1=6.3
-num2=3.0
-print(f"quotient of two numbers is {num1/num2}")
-
-word="Hello World"
-print(word)
-word="Goodbye World"
-print(word)
-
-value=True
-print(value)
-
-num1=7
-num2=2
-print(f"remainder of two numbers is {num1%num2}")
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
+#q6()
+#q7()
